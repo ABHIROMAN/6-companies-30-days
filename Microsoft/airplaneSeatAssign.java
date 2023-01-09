dace875f3d098f1a1cdc6552180102d857c766f5
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2023-01-09 23:39:28 +0530

    updating 13

---
+++ Microsoft/airplaneSeatAssign.java
@@ -0,0 +1,11 @@
+package Microsoft;
+
+public class airplaneSeatAssign {
+    
+    public double nthPersonGetsNthSeat(int n) {
+        if(n == 1) return 1;
+        else {
+            return 0.5;
+        }
+    }
+}
