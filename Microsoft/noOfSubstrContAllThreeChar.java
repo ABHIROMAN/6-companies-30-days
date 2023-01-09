6c3c5d30f57d43817fdb8d2cf8647353012f60e3
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2023-01-10 00:01:13 +0530

    update 15

---
+++ Microsoft/noOfSubstrContAllThreeChar.java
@@ -0,0 +1,23 @@
+package Microsoft;
+
+public class noOfSubstrContAllThreeChar {
+    
+    public int numberOfSubstrings(String s) {
+        
+        int left = 0;
+        int res = 0;
+
+        int arr[] = new int[3];
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+            arr[ch - 'a']++;
+            while(arr[0] > 0 && arr[1] > 0 && arr[2] > 0) {
+                res += s.length()-i;
+                arr[s.charAt(left++)-'a']--;
+            }
+        }
+        return res;
+    }
+}
