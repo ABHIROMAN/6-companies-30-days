e225c74ca7b36c9e24fa9cca073cab09dee36e8e
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2023-01-17 21:36:32 +0530

    updating factorial Trailing zeroes

--- GoldmanSachs/Validsquare.java
+++ GoldmanSachs/Validsquare.java
@@ -24,3 +24,4 @@ public class Validsquare {
         return (a[0]-b[0]) * (a[0]-b[0]) + (a[1]-b[1]) * (a[1]-b[1]);
     }
 }
+
---
+++ GoldmanSachs/factorialTrailingZeroes.java
@@ -0,0 +1,20 @@
+package GoldmanSachs;
+
+public class factorialTrailingZeroes {
+    
+    public int trailingZeroes(int n) {
+        
+        int num_zeros = 0;
+
+        for(int i = 5; i <= n; i += 5) {
+
+            int num = i;
+
+            while(num % 5 == 0) {
+                num /= 5;
+                num_zeros++;
+            }
+        }
+        return num_zeros;
+    }
+}
